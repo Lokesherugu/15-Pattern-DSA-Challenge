d25a002f654bab20531d3a4c012379e641f956a5
Lokesh Erugu <lokesherugu401@gmail.com>
2025-07-28 21:45:52 +0530

    Moved Java files into respective folders

---
+++ 01-Sliding-Window/template.java
@@ -0,0 +1,11 @@
+int left = 0;
+for (int right = 0; right < nums.length; right++) {
+    // Expand the window
+
+    // While condition not valid, shrink from left
+    while (/* condition */) {
+        left++;
+    }
+
+    // Possibly update answer
+}
